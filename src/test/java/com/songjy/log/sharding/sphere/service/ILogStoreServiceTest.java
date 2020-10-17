125fe0ba529fea696a1267a1e563bce83996170d
songjy <songjy@ipharmacare.net>
2020-10-17 23:30:13 +0800

    spring boot采用log4j2记录日志

--- src/test/java/com/songjy/log/sharding/sphere/service/ILogStoreServiceTest.java
+++ src/test/java/com/songjy/log/sharding/sphere/service/ILogStoreServiceTest.java
@@ -2,6 +2,7 @@ package com.songjy.log.sharding.sphere.service;
 
 import cn.hutool.core.lang.Assert;
 import com.songjy.log.sharding.sphere.model.LogStore;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ import java.util.List;
  * @date 2020-10-17
  */
 @SpringBootTest
+@Slf4j
 public class ILogStoreServiceTest {
 
     @Autowired
@@ -23,13 +25,14 @@ public class ILogStoreServiceTest {
     void selectByLogDateTest() {
         List<LogStore> logStores = logStoreService.selectByLogDate(LocalDate.now().minusDays(1L));
         Assert.notEmpty(logStores);
-        logStores.forEach(e -> System.out.println(e.getId()));
+        logStores.forEach(e -> log.info("主键>>{}", e.getId()));
     }
 
     @Test
     void selectBetweenLogDateTest() {
         List<LogStore> logStores = logStoreService.selectBetweenLogDate(LocalDate.now().minusDays(1L), LocalDate.now());
         Assert.notEmpty(logStores);
+        logStores.forEach(e -> log.info("主键>>{}", e.getId()));
     }
 
 }
